1bf7cf32fc79a044a20e9d77ecb84c774854873e
tjforeman <tylerforeman92@gmail.com>
2019-03-05 16:38:14 -0500

    added some eventlisteners

--- js/index.js
+++ js/index.js
@@ -1,12 +1,27 @@
 // Your code goes here
 const container= document.querySelector('.container');
-container.addEventListener('click',function(event){
+container.addEventListener('dblclick',function(event){
   event.target.style.color='blue';
 });
 const btn= document.getElementById('btn-1');
-btn.addEventListener('dblclick',function(event){
-  event.target.style.backgroundColor='black';
-  event.target.style.color='green';
+btn.addEventListener('click',function(event){
+  event.target.style.backgroundColor='red';
+  event.target.style.color='black';
+  console.log(`Fun in the sun has been clicked`)
+  event.stopPropagation();
+});
+const btn2= document.getElementById('btn-2');
+btn2.addEventListener('click',function(event){
+  event.target.style.backgroundColor='white';
+  event.target.style.color='black';
+  console.log(`Mountain Excursion has been clicked`)
+  event.stopPropagation();
+});
+const btn3= document.getElementById('btn-3');
+btn3.addEventListener('click',function(event){
+  event.target.style.backgroundColor='blue';
+  event.target.style.color='black';
+  console.log(`Island Getaways been clicked`)
   event.stopPropagation();
 });
 
@@ -15,3 +30,10 @@ destination.addEventListener('mouseover',function(event){
   event.target.style.color='green';
   event.stopPropagation();
 });
+
+window.addEventListener("load", function(event) {
+    console.log("The page has loaded!");
+  });
+  window.addEventListener("keydown", function(event) {
+    console.log(`A key was pressed ! this is the key: ${event.key}`);
+  });
