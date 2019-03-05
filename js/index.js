b98a7e6b7304336658e79db9a10bd62a4520a296
tjforeman <tylerforeman92@gmail.com>
2019-03-05 15:55:53 -0500

    inital commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+const container= document.querySelector('.container');
+container.addEventListener('click',function(event){
+  event.target.style.color='blue';
+});
+const btn= document.getElementById('btn-1');
+btn.addEventListener('dblclick',function(event){
+  event.target.style.backgroundColor='black';
+  event.target.style.color='green';
+  event.stopPropagation();
+});
+
+const destination= document.querySelector('.text-content');
+destination.addEventListener('mouseover',function(event){
+  event.target.style.color='green';
+  event.stopPropagation();
+});
